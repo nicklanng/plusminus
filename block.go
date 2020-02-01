91c7b7fdb20eab01aae8c234c33c20cd950ed329
nicklanng <rinse@rinseworld.com>
2020-02-01 13:53:45 +0000

    Introduce top level query with query blocks

---
+++ block.go
@@ -0,0 +1,34 @@
+package plusminus
+
+// Block is a query block that can make up part of a query.
+func Block(name string, function expr) *block {
+	return &block{
+		name:     name,
+		function: function,
+	}
+}
+
+type block struct {
+	name       string
+	function   expr
+	normalize  bool
+	predicates predicateList
+}
+
+// Normalize will toggle on normalization of predicate and facet results.
+// Only predicates and facets with an alias will be returned.
+func (q *block) Normalize() *block {
+	q.normalize = true
+	return q
+}
+
+// Predicates is the list of predicates to return from the current node.
+// Predicates can be nested.
+func (q *block) Predicates(preds ...*predicate) *block {
+	q.predicates = append(q.predicates, preds...)
+	return q
+}
+
+func (q block) toString() string {
+	return q.name + "(func: " + q.function.toString() + ") " + q.predicates.toString() + "\n"
+}
--- query.go
+++ query.go
@@ -1,34 +1,23 @@
 package plusminus
 
-// Query is the top level structure of a dgraph query.
-func Query(name string, function expr) *query {
-	return &query{
-		name:     name,
-		function: function,
+func Query(blocks ...*block) query {
+	return query{
+		blocks: blocks,
 	}
 }
 
 type query struct {
-	name       string
-	function   expr
-	normalize  bool
-	predicates predicateList
-}
-
-// Normalize will toggle on normalization of predicate and facet results.
-// Only predicates and facets with an alias will be returned.
-func (q *query) Normalize() *query {
-	q.normalize = true
-	return q
-}
-
-// Predicates is the list of predicates to return from the current node.
-// Predicates can be nested.
-func (q *query) Predicates(preds ...*predicate) *query {
-	q.predicates = append(q.predicates, preds...)
-	return q
+	blocks []*block
 }
 
 func (q query) ToString() string {
-	return q.name + "(func: " + q.function.toString() + ") " + q.predicates.toString() + "\n"
+	s := "{\n"
+
+	for i := range q.blocks {
+		s += q.blocks[i].toString()
+	}
+
+	s += "}"
+
+	return s
 }
