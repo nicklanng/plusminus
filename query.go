ab0f5f95687f2cede5e2af10e32813b2b220b82f
nicklanng <rinse@rinseworld.com>
2020-02-02 01:15:19 +0000

    Renamed ToString() -> String() & added StringIndented() to query for producing human readable query strings

--- query.go
+++ query.go
@@ -1,6 +1,9 @@
 package plusminus
 
-import "strings"
+import (
+	"bytes"
+	"strings"
+)
 
 // Query is a top-level query to dgraph.
 func Query(name string) *query {
@@ -27,8 +30,8 @@ func (q *query) Blocks(b ...*block) *query {
 	return q
 }
 
-// ToString creates a string representation of the whole query, which can be used to query dgraph.
-func (q *query) ToString() string {
+// String creates a string representation of the whole query, which can be used to query dgraph.
+func (q *query) String() string {
 	s := "query " + q.name
 
 	if len(q.variables) > 0 {
@@ -42,4 +45,35 @@ func (q *query) ToString() string {
 	s += "}"
 
 	return s
+}
+
+// StringIndented creates a query string like String(), but with appropriate indentation.
+// This is not required for dgraph to parse the query and it is more computationally expensive, so use only when human readability of the query is desired.
+func (q *query) StringIndented() string {
+	var (
+		buf    = new(bytes.Buffer)
+		depth  = 0
+		indent = []byte{' ', ' '}
+	)
+
+	str := q.String()
+
+	for i := range str {
+		if str[i] == '{' {
+			depth++
+		}
+
+		if i+1 < len(str) && str[i+1] == '}' {
+			depth--
+		}
+
+		buf.WriteByte(str[i])
+
+		if str[i] == '\n' {
+			buf.Write(bytes.Repeat(indent, depth))
+		}
+
+	}
+
+	return buf.String()
 }
